dbac64fe97b13a36eb67b56d39c418294fb75469
Justin Lampe <pierrelampre@gmail.com>
2019-11-26 21:13:20 -0500

    edited connection.js with conditional for jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,12 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
+if(process.env.JAWSDB_URL) {
+
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+} else {
+
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -8,6 +14,8 @@ var connection = mysql.createConnection({
   password: "stayoutmyshit3#",
   database: "burger_db"
 });
+
+}
 
 // Make connection.
 connection.connect(function(err) {
